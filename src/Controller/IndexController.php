55835c5e86c305ee4cd4d5fd9cc9f09a3ae7b0d1
agathism <soumare.aicha13@gmail.com>
2025-05-05 19:16:54 +0200

    configuration des routes

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,4 +13,34 @@ final class IndexController extends AbstractController
     {
         return $this->render('index/home.html.twig');
     }
+
+    #[Route('/heroes', name: 'app_heroes')]
+    public function index(): Response
+    {
+        return $this->render('index/heroes.html.twig');
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(): Response
+    {
+        return $this->render('index/login.html.twig');
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): Response
+    {
+        return $this->render('index/logout.html.twig');
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('index/profile.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('index/contact.html.twig');
+    }
 }
