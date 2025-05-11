75d96ac88ae4356780b68183d5c87a2b343b05b4
agathism <soumare.aicha13@gmail.com>
2025-05-11 22:20:11 +0200

    🎨 doing some frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,40 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Couleurs principales
+        'ow-blue': '#AFDDFF',
+        'ow-violet': '#8A2BE2',
+        'ow-pink': '#FF6AD5',
+        'ow-dark-blue': '#AFDDFF',
+        // Couleurs neutres
+        'ow-white': '#FFFFFF',
+        'ow-light-gray': '#F5F5F7',
+      },
+      animation: {
+        'blob-bounce': 'blob-bounce 5s infinite ease',
+      },
+      keyframes: {
+        'blob-bounce': {
+          '0%': {
+            transform: 'translate(-100%, -100%) translate3d(0, 0, 0)',
+          },
+          '25%': {
+            transform: 'translate(-100%, -100%) translate3d(100%, 0, 0)',
+          },
+          '50%': {
+            transform: 'translate(-100%, -100%) translate3d(100%, 100%, 0)',
+          },
+          '75%': {
+            transform: 'translate(-100%, -100%) translate3d(0, 100%, 0)',
+          },
+          '100%': {
+            transform: 'translate(-100%, -100%) translate3d(0, 0, 0)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
